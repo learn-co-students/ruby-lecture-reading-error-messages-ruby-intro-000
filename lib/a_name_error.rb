c4d9ac548814dc5ce450d4d01ed69f2988b1549f
Alex Williams <awbeacon@gmail.com>
2016-08-05 17:38:27 +0000

    Done.

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,22 @@
 "hello world"
 
-hello_world
+hello_world = "test"
+puts hello_world
+5.times do
+  puts hello_world
+  hello_world+="x"
+end
+$x=0
+until $x > 10 do
+  puts hello_world
+  hello_world+="y"
+  $x+=1
+end
+
+$i = 0
+$num = 5
+
+until $i > $num  do
+   puts("Inside the loop i = #$i" )
+   $i +=1;
+end
