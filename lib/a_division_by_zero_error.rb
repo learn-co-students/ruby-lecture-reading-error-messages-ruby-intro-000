6526c295b172507ecaa1859aa32ff126bacd858c
Joaquin Esquilin <esquilin.joaquin@gmail.com>
2020-01-11 16:47:23 +0000

    Done.

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -1,3 +1,8 @@
-42 / 6
-
-42 / 0
+likes_ruby = TRUE #boolean
+x = 1 # Integer
+name "Jacky" # String
+favorite_number = [1,7,21] # array
+names = ["Jacky", "Mike", "John"] # array
+# If I wanted to get the 7 out of these numbers then 
+puts favorite_number [1]
+puts names[2]
