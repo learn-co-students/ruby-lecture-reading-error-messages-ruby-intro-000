8940a6fcd19247aa0b7187a915671891035ddec2
Jordan Reed <jordanalireed@icloud.com>
2020-06-15 04:16:01 +0000

    Done.

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -1,3 +1,12 @@
-42 / 6
 
-42 / 0
+ #Set the numerator.
+ "'numerator=42'
+ #Set the denominator.
+ 'denominator=6'
+ #Divide and output the result
+ logging.log(numerator / denominator)"
+ "'numerator=42'
+ #Set the denominator.
+ 'denominator=0'
+ #Divide and output the result
+ logging.log(numerator / denominator)"
